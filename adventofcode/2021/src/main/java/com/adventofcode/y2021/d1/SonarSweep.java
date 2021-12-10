f9f3edb52217914de7d220bbf8069cd42e301808
st-angeles <95905818+st-angeles@users.noreply.github.com>
2021-12-10 09:50:23 +0100

    Add logic

--- adventofcode/2021/src/main/java/com/adventofcode/y2021/d1/SonarSweep.java
+++ adventofcode/2021/src/main/java/com/adventofcode/y2021/d1/SonarSweep.java
@@ -8,9 +8,31 @@ public class SonarSweep {
 
     private SonarSweepReport sonarSweepReport;
 
- public long countIncreases(){
-     return 0;
- }
+    public long countIncreases() throws  RuntimeException{
+        if(sonarSweepReport == null || !( sonarSweepReport.getSeaFloorDepths().size() >= 2)) {
+            throw new RuntimeException("Insufficient sweeps !");
+        }
+        else
+        {
+            List<Integer> seaFloorDepths = sonarSweepReport.getSeaFloorDepths();
+            long count = seaFloorDepths.stream()
+                                       .map(this::IncDecMapper)
+                                       .filter(x -> x.equals("increased"))
+                                       .count();
+            return count;
+        }
+    }
+
+    public String IncDecMapper(int x){
+        List<Integer>  seaFloorDepths = sonarSweepReport.getSeaFloorDepths();
+        int idx = seaFloorDepths.indexOf(x);
+        if(idx == 0){
+            return "none";
+        } else {
+            int depthDiff = x - seaFloorDepths.get(idx - 1);
+            return  (depthDiff > 0) ? "increased" : "decreased";
+        }
+    }
 
     public void setSonarSweepReport(SonarSweepReport sonarSweepReport) {
         this.sonarSweepReport = sonarSweepReport;
