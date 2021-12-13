370566fa235c68cc2f579d188fc87bba158aa26a
ICONOS\stefaan <Angelescu.Stefan@cronos.be>
2021-12-13 11:54:42 +0100

    Update the logic to pass the failing test

--- adventofcode/2021/src/main/java/com/adventofcode/y2021/d1/SonarSweep.java
+++ adventofcode/2021/src/main/java/com/adventofcode/y2021/d1/SonarSweep.java
@@ -1,37 +1,67 @@
 package com.adventofcode.y2021.d1;
-
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.ArrayList;
 
 public class SonarSweep {
 
+    public enum SweepLevel{
+        N_A,
+        DECREASED,
+        INCREASED
+    }
+
+    class Counter{
+        private int counter;
+        private int SIZE;
+
+        public Counter(){
+            SIZE = 1;
+            counter = 0;
+        }
+
+        public Counter(int size){
+            SIZE = size;
+            counter = 0;
+        }
+
+        public void inc(){
+            counter = (counter + 1 ) % SIZE;
+        }
+
+        public int value(){
+            return counter;
+        }
+    }
+
     private SonarSweepReport sonarSweepReport;
 
-    public long countIncreases() throws  RuntimeException{
+    long countIncreases() throws  RuntimeException{
         if(sonarSweepReport == null || !( sonarSweepReport.getSeaFloorDepths().size() >= 2)) {
             throw new RuntimeException("Insufficient sweeps !");
         }
         else
         {
             List<Integer> seaFloorDepths = sonarSweepReport.getSeaFloorDepths();
+            Counter idx = new Counter(seaFloorDepths.size());
             long count = seaFloorDepths.stream()
-                                       .map(this::IncDecMapper)
-                                       .filter(x -> x.equals("increased"))
-                                       .count();
+                    .map( (x) -> SweepLevelMapper(x, idx, seaFloorDepths))
+                    .filter(x -> x == SweepLevel.INCREASED)
+                    .count();
             return count;
         }
     }
 
-    public String IncDecMapper(int x){
-        List<Integer>  seaFloorDepths = sonarSweepReport.getSeaFloorDepths();
-        int idx = seaFloorDepths.indexOf(x);
-        if(idx == 0){
-            return "none";
+    public static SweepLevel SweepLevelMapper(Integer x, Counter idx, List<Integer> depths){
+        SweepLevel level;
+        if(idx.value() == 0){
+            level = SweepLevel.N_A;
         } else {
-            int depthDiff = x - seaFloorDepths.get(idx - 1);
-            return  (depthDiff > 0) ? "increased" : "decreased";
+            int depthDiff = x - depths.get(idx.value() - 1);
+            level =  (depthDiff > 0) ? SweepLevel.INCREASED : SweepLevel.DECREASED;
         }
+        idx.inc();
+
+        return level;
     }
 
     public void setSonarSweepReport(SonarSweepReport sonarSweepReport) {
